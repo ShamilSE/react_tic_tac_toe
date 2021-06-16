fb7560cf33a63d5f0cb40a4877871a0a402b13f0
Mussels Ismene <mismene@si-g1.kzn.21-school.ru>
2021-06-16 16:21:23 +0300

    x and o moving, determine winner

--- src/Board.js
+++ src/Board.js
@@ -5,15 +5,26 @@ class Board extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            squares: Array(9).fill(null)
+            squares: Array(9).fill(null),
+            sign: "X",
+            winner: null,
         }
     }
 
     handleClick(squareNo) {
-        console.log("hello")
         let newSquares = this.state.squares.slice()
-        newSquares[squareNo] = "X"
+        newSquares[squareNo] = this.state.sign
+
+        if (this.state.sign === "X")
+            this.setState({sign: "O"})
+        else
+            this.setState({sign: "X"})
+
         this.setState({squares: newSquares})
+
+        let winner = this.calculateWinner(this.state.squares)
+        if (winner)
+            this.setState({winner})
     }
 
     renderSquare(i) {
@@ -23,12 +34,56 @@ class Board extends React.Component {
         />;
     }
 
+    getStatus() {
+        let status = 'Next player: ' + this.state.sign;
+        let index = 0
+        let counter = 0
+
+        while (index < 9) {
+            if (this.state.squares[index] != null)
+                counter++
+            index++
+        }
+        if (counter === 9 || this.state.winner)
+            status = "Game over"
+        return status
+    }
+
+    newGame() {
+        let newSquares = this.state.squares.slice()
+
+        newSquares.fill(null)
+        this.setState({squares: newSquares, sign: "X", winner: null})
+    }
+
+    calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
+
     render() {
-        const status = 'Next player: X';
+        const status = this.getStatus()
 
         return (
             <div>
                 <div className="status">{status}</div>
+                <div>{this.state.winner ? "winner is " + this.state.winner : null}</div>
+                <button onClick={() => this.newGame()}>new game</button>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}

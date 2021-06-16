4373be39361c116bd6cf908005e1dd8ebfd35d86
Mussels Ismene <mismene@si-g1.kzn.21-school.ru>
2021-06-16 15:01:31 +0300

    up state to a Board component

--- src/index.js
+++ src/index.js
@@ -1,62 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-class Square extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            value: 0
-        }
-    }
-
-    clickSquare(squreNo) {
-        if (this.state.value === 0)
-            this.state.value = 1
-        else
-            this.state.value = 0
-        console.log("click " + squreNo + " state: " + this.state.value)
-    }
-
-    render() {
-        return (
-            <button className="square" onClick={() => this.clickSquare(this.props.value)}>
-                {this.props.value} === 0 ? {this.props.value} : "X"
-            </button>
-        );
-    }
-}
-
-class Board extends React.Component {
-    renderSquare(i) {
-        return <Square value={i}  />;
-    }
-
-    render() {
-        const status = 'Next player: X';
-
-        return (
-            <div>
-                <div className="status">{status}</div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-        );
-    }
-}
+import {Board} from './Board'
 
 class Game extends React.Component {
     render() {
